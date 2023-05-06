939e2375e44c7c8d70631bab0df108b8b717af94
AlexandrePDA <a.pires130@gmail.com>
2023-05-06 16:19:50 +0200

    add arrow

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,6 +14,7 @@ import Test from '../Components/Test.js';
 
 
 
+
 const Home = () => {
     return (
         <div className="font-montserrat overflow-x-hidden">
@@ -21,6 +22,11 @@ const Home = () => {
             <div className="2xl:hidden flex justify-center opacity-70 mx-auto mt-[-2rem]">
             <Test/>
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+            className="w-12 h-12 2xl:hidden flex justify-center opacity-70 mx-auto mt-[-2rem] element">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+
             <Accueil/>
             <Collaborations/> 
             <Techniques />
